176bc433977b670eb2c75a6af06e46ae0b2ace0d
yerincho94 <yerincho94@gmail.com>
2025-09-30 12:47:54 +0900

    주석 추가

---
+++ README.md
@@ -0,0 +1,8 @@
+```sh
+# npm init -y : 이건 할 필요 없고,
+# git clone, git pull로 했다면
+npm install # package.json
+npm run dev # 3000
+# 다른데서 3000을 사용하고 있으면 중복 에러가 뜬다
+# 종료하고 싶으면 cntrl + c (cancel의 의미)
+```
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express = require("express");
-const app = express();
+const express = require("express"); //npm install express을 불러와서 쓴다는 의미 (의존성 -> 설치해서 사용)
+const app = express(); //만들어진 객체
 const port = 3000;
 
+//get 요청 -> fetch, get/post
 app.get("/", (req, res) => {
+  //localhost:3000/ -> 브라우저를 통한 접속
   //res.send("Hello World!");
   res.send("Bye Earth!");
 });
 
+//listen을 통해서 서버 구동 -> 구동 시킨 서버가 localhost:3000경로와 연결됨.
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
